ea95c8a8c3768e10481c954da61efd46e4c0f3f9
Mark Lubkowitz <mail@mark-lubkowitz.de>
2021-11-02 17:00:04 +0100

    Todo list done, comments fixed

--- trimfandango.js
+++ trimfandango.js
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 
 module.exports = function trimFandango (text, charlist = ["\0", "\t", "\n", "\x0B", "\r", " "]) {
-    //  charlist == string: find full set and subsets left and right and remove them
+    
     //  charlist == array: every element of the list left and right will be removed
-    //  it always starts left, then right
-    //  TODO: how to handle empty arrays?
-    //  TODO: how to handle empty strings?
-
-    //  it's an array. create the regular expressions to find a group of matched characters
     let reTrimLeft = null,
-        reTrimRight = null
+    reTrimRight = null
     if (Array.isArray(charlist)) {
         let reCharlist  = `[${charlist.join()}]+`
         reTrimLeft      = new RegExp('^' + reCharlist, 'iu')
         reTrimRight     = new RegExp(reCharlist + '$', 'iu')
     } else {
-        //  it's not an array, so force the value of charlist to String.
+        //  charlist == string: find full set and subsets left and right and remove them
         let subsetLeft  = [],
         subsetRight     = []
         charlist        = String(charlist)
@@ -30,7 +25,7 @@ module.exports = function trimFandango (text, charlist = ["\0", "\t", "\n", "\x0
         }
         reTrimRight     = new RegExp(`(?:${subsetRight.join('|')})$`)
     }    
-
+    
     //  trim the text, first from the start (left), second from the end (right)
     return text.replace(reTrimLeft,'').replace(reTrimRight,'')
 }
